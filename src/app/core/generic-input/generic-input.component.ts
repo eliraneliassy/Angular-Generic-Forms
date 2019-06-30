7493915c904653656fa64d0c1c1cd4c339f89ebf
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-06-30 12:20:44 +0300

    starting point

--- src/app/core/generic-input/generic-input.component.ts
+++ src/app/core/generic-input/generic-input.component.ts
@@ -1,29 +1,15 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, Self, Optional, forwardRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import {
-  ControlValueAccessor, NG_VALUE_ACCESSOR, Validator, AbstractControl,
-  ValidatorFn, Validators, NG_VALIDATORS, NgControl, NgForm
+  ControlValueAccessor
 } from '@angular/forms';
 
 @Component({
   selector: 'app-generic-input',
   templateUrl: './generic-input.component.html',
-  styleUrls: ['./generic-input.component.scss'],
-  // providers: [{
-  //   provide: NG_VALUE_ACCESSOR,
-  //   multi: true,
-  //   useExisting: GenericInputComponent
-  // },
-  // {
-  //   provide: NG_VALIDATORS,
-  //   multi: true,
-  //   useExisting: GenericInputComponent
-  // }]
+  styleUrls: ['./generic-input.component.scss']
 })
-export class GenericInputComponent implements ControlValueAccessor, Validator, OnInit {
+export class GenericInputComponent {
 
-
-  @ViewChild('input') input: ElementRef;
-  disabled;
 
   @Input() type = 'text';
   @Input() isRequired: boolean = false;
@@ -31,53 +17,4 @@ export class GenericInputComponent implements ControlValueAccessor, Validator, O
   @Input() label: string = null;
   @Input() placeholder: string;
   @Input() errorMsg: string;
-
-  constructor(@Self() public controlDir: NgControl) {
-    this.controlDir.valueAccessor = this;
-  }
-
-  ngOnInit() {
-    const control = this.controlDir.control;
-    const validators: ValidatorFn[] = control.validator ? [control.validator] : [];
-    if (this.isRequired) {
-      validators.push(Validators.required);
-    }
-    if (this.pattern) {
-      validators.push(Validators.pattern(this.pattern));
-    }
-
-    control.setValidators(validators);
-    control.updateValueAndValidity();
-  }
-
-  onChange(event) { }
-
-  onTouched() { }
-
-  writeValue(obj: any): void {
-    this.input.nativeElement.value = obj;
-  }
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
-  validate(c: AbstractControl): { [key: string]: any; } {
-    const validators: ValidatorFn[] = [];
-    if (this.isRequired) {
-      validators.push(Validators.required);
-    }
-    if (this.pattern) {
-      validators.push(Validators.pattern(this.pattern));
-    }
-
-    return validators;
-  }
-
-
 }
