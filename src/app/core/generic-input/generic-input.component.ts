817c7abdd77e8ad96d3598fe5941d34cee5e88ff
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-06-30 12:34:49 +0300

    control value accessor

--- src/app/core/generic-input/generic-input.component.ts
+++ src/app/core/generic-input/generic-input.component.ts
@@ -1,20 +1,44 @@
 import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import {
-  ControlValueAccessor
+  ControlValueAccessor, NG_VALUE_ACCESSOR
 } from '@angular/forms';
 
 @Component({
   selector: 'app-generic-input',
   templateUrl: './generic-input.component.html',
-  styleUrls: ['./generic-input.component.scss']
+  styleUrls: ['./generic-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: GenericInputComponent,
+      multi: true
+    }
+  ]
 })
-export class GenericInputComponent {
+export class GenericInputComponent implements ControlValueAccessor {
 
-
+  @ViewChild('input') input: ElementRef;
   @Input() type = 'text';
   @Input() isRequired: boolean = false;
   @Input() pattern: string = null;
   @Input() label: string = null;
   @Input() placeholder: string;
   @Input() errorMsg: string;
+  disabled: boolean;
+
+  onChange(event) { }
+  onTouched() { }
+
+  writeValue(obj: any): void {
+    this.input.nativeElement.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
